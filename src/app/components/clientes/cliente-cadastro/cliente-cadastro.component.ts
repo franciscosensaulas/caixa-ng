378ca2365aec059b037c93f683604c835ac36a6b
Francisco Lucas Janesch Lange Sens <franciscosensaulas@gmail.com>
2025-05-05 22:10:12 -0300

    Aula 2025-05-05

--- src/app/components/clientes/cliente-cadastro/cliente-cadastro.component.ts
+++ src/app/components/clientes/cliente-cadastro/cliente-cadastro.component.ts
@@ -1,11 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Cliente } from '../../../models/cliente';
 
 @Component({
   selector: 'app-cliente-cadastro',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './cliente-cadastro.component.html',
   styleUrl: './cliente-cadastro.component.css'
 })
 export class ClienteCadastroComponent {
+  // Evento de saída que permite ao componente filho (ClienteCadastroComponent)
+  // enviar uma string para o componente pai (ClienteComponent)
+  @Output() salvarEvento = new EventEmitter<void>();
 
+  // O componente pai vai passar o objeto do cliente de acordo com modo:
+  // - modo de cadastro os campos do cliente estarão vazios
+  // - modo de editar os campos do cliente estarão preenchidos
+  @Input() cliente?: Cliente;
+  
+  salvar() {
+    this.salvarEvento.emit();
+  }
 }
--- src/app/components/clientes/cliente/cliente.component.ts
+++ src/app/components/clientes/cliente/cliente.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { ClienteCadastroComponent } from "../cliente-cadastro/cliente-cadastro.component";
+import { Cliente } from '../../../models/cliente';
+import { max, reduce } from 'rxjs';
 
 @Component({
   selector: 'app-cliente',
@@ -8,5 +10,84 @@ import { ClienteCadastroComponent } from "../cliente-cadastro/cliente-cadastro.c
   styleUrl: './cliente.component.css'
 })
 export class ClienteComponent {
+  clientes: Array<Cliente> = new Array();
+  idAtual: number = 0;
 
+  // Será o cliente que utilizaremos para preencher os campos na tela e posteriormente salvar
+  cliente: Cliente;
+
+  constructor() {
+    this.cliente = new Cliente();
+  }
+
+  // evento que é executado quando o componente é instanciado
+  ngOnInit() {
+    this.carregarClientesDoLocalStorage();
+  }
+
+  registrarClienteSalvo() {
+    if (this.cliente.id === 0)
+      this.cadastrar();
+    else
+      this.editar();
+
+    this.cliente = new Cliente();
+    this.salvarEmLocalStorage();
+  }
+
+  private editar(){
+    let indiceCliente = this.clientes.findIndex(x => x.id == this.cliente.id);
+    this.clientes[indiceCliente].nome = this.cliente.nome;
+  }
+
+  private cadastrar() {
+    this.idAtual++;
+
+    this.cliente.id = this.idAtual;
+
+    // Adicionando este objeto na lista de clientes
+    this.clientes.push(this.cliente);
+  }
+
+  salvarEmLocalStorage() {
+    // this.clientes é uma lista de objetos da classe Clientes
+    // JSON.stringify é uma função para converter listas/objetos para string no formato JSON
+    const clientesString = JSON.stringify(this.clientes);
+    // Armazenando utilizando a chave 'clientes' a string com a lista de clientes
+    localStorage.setItem("clientes", clientesString);
+  }
+
+  carregarClientesDoLocalStorage() {
+    // Obter do localStorage(armazenamento no navegador) utilizando a chave clientes
+    const clientesString = localStorage.getItem("clientes");
+    // Verificar senão existe a lista de clientes no LocalStorage
+    if (clientesString === null)
+      // Encerra a execução da função, pois n existe lista armazenada
+      return;
+    // Converter a string(JSON) para lista de objetos
+    this.clientes = JSON.parse(clientesString);
+    // Percorre cada um dos clientes para atualizar o idAtual com o maior id dos clientes cadastrados
+    Array.from(this.clientes).forEach(cliente => {
+      if (cliente.id > this.idAtual) {
+        this.idAtual = cliente.id
+      }
+    });
+  }
+
+  apagar(cliente: Cliente) {
+    let confirmacao = confirm(`Deseja realmente apagar o cliente'${cliente.nome}'?`);
+    if (confirmacao !== true)
+      return;
+
+    let indicecliente = this.clientes.findIndex(x => x.id == cliente.id);
+    this.clientes.splice(indicecliente, 1);
+
+    this.salvarEmLocalStorage();
+  }
+
+  preencherCamposParaEditar(cliente: Cliente) {
+    this.cliente = new Cliente();
+    this.cliente.id = cliente.id;
+    this.cliente.nome = cliente.nome;
+  }
 }
